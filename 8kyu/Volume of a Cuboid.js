e58e0aa0cb0d7f575ae04c74d9ec8b50934e5a9b
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-16 12:52:16 +0300

    Add task solution

---
+++ 8kyu/Volume of a Cuboid.js
@@ -0,0 +1,12 @@
+//Bob needs a fast way to calculate the volume of a cuboid with three values: length,
+// width and the height of the cuboid. Write a function to help Bob with this calculation.
+
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+        console.log(length*width*height);
+    }
+}
+
+Kata.getVolumeOfCuboid(1,2,2);
+Kata.getVolumeOfCuboid(6.3,2,5);
+Kata.getVolumeOfCuboid(2,2.5,3.5);
