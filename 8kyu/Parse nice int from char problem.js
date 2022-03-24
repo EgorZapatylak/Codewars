841cb8f82212b01be795bcde74e2e82a9c30e8ae
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-24 14:54:42 +0300

    Create tasks solution

---
+++ 8kyu/Parse nice int from char problem.js
@@ -0,0 +1,17 @@
+//Ask a small girl - "How old are you?". She always says strange things... Lets help her!
+//
+// For correct answer program should return int from 0 to 9.
+//
+// Assume test input string always valid and may look like "1 year old" or "5 years old", etc.. The first char is number only.
+
+function getAge(inputString) {
+
+    console.log(Number(inputString.split('')[0]));
+
+}
+
+getAge("4 years old");
+getAge("5 years old");
+getAge("1 years old");
+getAge("2 years old");
+getAge("3 years old");
