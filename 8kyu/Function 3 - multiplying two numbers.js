cff8650d49f183ea7d3d07349b2a9832101286ce
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-24 14:30:31 +0300

    Create tasks solution

---
+++ 8kyu/Function 3 - multiplying two numbers.js
@@ -0,0 +1,9 @@
+//Implement a function which multiplies two numbers.
+
+const multiply = (a, b) => a * b;
+
+console.log(multiply(2, 3));
+console.log(multiply(10, 5));
+console.log(multiply(-2, 3));
+console.log(multiply(0, 3));
+console.log(multiply(8888, 55));
