29be7562dd3dc63b28bb9ab7336bd4ef681229f2
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-28 08:20:29 +0300

    Create tasks solution

---
+++ 8kyu/Convert a string to an array.js
@@ -0,0 +1,12 @@
+//Write a function to split a string and convert it into an array of words.
+//
+// * "Robin Singh" ==> ["Robin", "Singh"]
+//
+// * "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+function stringToArray(string) {
+    console.log(string.split(' '));
+}
+
+stringToArray("Robin Singh");
+stringToArray("I love arrays they are my favorite");
