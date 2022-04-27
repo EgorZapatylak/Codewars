85d325083718e0a136bedd49b344a02e37353da1
EgorZapatylak <zapatylak96a@gmail.com>
2022-04-27 14:13:56 +0300

    Create tasks solution

---
+++ 8kyu/Bin to Decimal.js
@@ -0,0 +1,11 @@
+//Complete the function which converts a binary number (given as a string) to a decimal number.
+
+function binToDec(bin) {
+    let a = parseInt(bin, 2)
+    return a
+}
+
+console.log(binToDec(101110));
+console.log(binToDec(0));
+console.log(binToDec(1001001));
+console.log(binToDec(1));
