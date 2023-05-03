26e35a5129b2dfc20ecd85ae8f2749dfc9cb2401
EgorZapatylak <zapatylak96a@gmail.com>
2023-05-03 22:01:55 +0300

    Create tasks solution

---
+++ 8kyu/Double Integer.js
@@ -0,0 +1,9 @@
+// Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i) {
+    return i * i;
+}
+
+console.log(doubleInteger(2));
+console.log(doubleInteger(5));
+console.log(doubleInteger(8));
